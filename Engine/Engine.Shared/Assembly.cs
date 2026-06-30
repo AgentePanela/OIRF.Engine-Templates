e7250e13efba20483ac6654264a2228bd3b59137
AgentePanela <AgentePanela@gmail.com>
2026-06-30 14:26:28 -0300

    replace Engine folder with OIRF.Engine submodule
    Tracks master, pinned at the 0.2.0 commit.

--- Engine/Engine.Shared/Assembly.cs
+++
@@ -1,5 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("YAML.Linter")]
-[assembly: InternalsVisibleTo("Engine.Client")]
-[assembly: InternalsVisibleTo("Engine.Shared")]
--- Engine/Engine.Shared/GameObjects/Attributes.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Engine.Shared.GameObjects;
-
-[AttributeUsage(AttributeTargets.Class, Inherited = false)]
-public sealed class RegisterComponentAttribute(string name) : Attribute
-{
-    public string Name => name;
-}
